f31247a02be2edf7222d2dc4ffb9ba17e02c79b9
Carlos de LeÓN <daniel@tukanstudios.com.gt>
2026-03-02 06:01:35 -0600

    archivo configu

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,17 @@
 module.exports = {
-  apps: [{
-    name: "scraper-n8n",
-    script: "index.js",
-    cron_restart: "*/20 * * * *",
-    cwd: "/www/wwwroot/scraper-n8n" // opcional si no lanzas desde esa carpeta
-  }]
-}
+  apps: [
+    {
+      name: "scraper-n8n",
+      // CAMBIO: Apunta a la carpeta dist donde está el código procesado
+      script: "./dist/index.js", 
+      cron_restart: "*/20 * * * *",
+      cwd: "/www/wwwroot/scraper-n8n",
+      // Opcional: Recomendado para manejar errores
+      env: {
+        NODE_ENV: "production"
+      }
+    }
+  ]
+};
+
+// prueba de publiacion
